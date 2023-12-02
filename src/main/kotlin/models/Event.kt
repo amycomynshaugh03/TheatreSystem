2ed394d7d6be63a4d5bf8452ad91008a47af846b
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 19:36:55 +0000

    Linking Event and Booking & addBooking added into Event

--- src/main/kotlin/models/Event.kt
+++ src/main/kotlin/models/Event.kt
@@ -6,5 +6,14 @@ data class Event( var eventId: Int = 0,
                   var eventDescription: String,
                   var ageRating: Int = 0,
                   var ticketPrice: Int = 0,
-                  var eventDuration: Int = 0
-)
+                  var eventDuration: Int = 0,
+                  var booking : MutableSet<Booking> = mutableSetOf()
+) {
+    private var lastBookingId = 0
+    private fun getBookingId() = lastBookingId++
+
+    fun addBooking( booking: Booking) : Boolean {
+        booking.bookingId = getBookingId()
+        return booking.add(booking)
+    }
+}
