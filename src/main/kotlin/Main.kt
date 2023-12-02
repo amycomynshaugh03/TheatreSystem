76a7720a3443dd29f10425ecad49fabd9fb64946
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 14:19:31 +0000

    Declaring functions in runMenu()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import controllers.TheatreAPI
+import models.Event
+import models.Booking
+import utils.ScannerInput.readNextChar
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+private val theatreAPI = TheatreAPI()
+
+fun runMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> addEvent()
+            2 -> listEvent()
+            3 -> updateEvent()
+            4 -> deleteEvent()
+            5 -> archiveEvent()
+            6 -> addBookingToEvent()
+            7 -> updateBookingContentsInEvent()
+            8 -> deleteBooking()
+            9 -> markItemStatus()
+            10 -> searchEvents()
+            15 -> searchBooking()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
 }
+
+
