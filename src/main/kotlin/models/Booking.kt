dcb3d0cb14fb2d45041a042c62c79491deb512f1
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 19:30:44 +0000

    updating booking data class to have bookingContents & isPaymentComplete

--- src/main/kotlin/models/Booking.kt
+++ src/main/kotlin/models/Booking.kt
@@ -7,5 +7,16 @@ data class Booking(var bookingId: Int = 0,
                    var customerName: String,
                    var customerPhone: Int = 0,
                    var paymentMethod: String,
-                   var paymentStatus: String
-)
+                   var paymentStatus: String,
+                   var bookingContents: String,
+                   var isPaymentComplete: Boolean = false
+) {
+    override fun toString(): String {
+        if (isPaymentComplete)
+            return "$bookingId: $bookingContents (Paid)"
+        else
+            return "$bookingId: $bookingContents (Unpaid)"
+    }
+
+
+}
