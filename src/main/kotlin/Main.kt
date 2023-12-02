fe702f69250dca3d555eac5b55ebee37ecaddeb4
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 22:41:25 +0000

    Deleting archiveEvent due to not pursuing it anymore and edited menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,13 +15,12 @@ fun runMenu() {
             2 -> listEvent()
             3 -> updateEvent()
             4 -> deleteEvent()
-            5 -> archiveEvent()
-            6 -> addBookingToEvent()
-            7 -> updateBookingContentsInEvent()
-            8 -> deleteBooking()
-            9 -> markPaymentStatus()
-            10 -> searchEvents()
-            15 -> searchBooking()
+            5 -> addBookingToEvent()
+            6 -> updateBookingContentsInEvent()
+            7 -> deleteBooking()
+            8 -> markPaymentStatus()
+            9 -> searchEvents()
+            10 -> searchBooking()
             0 -> exitApp()
             else -> println("Invalid menu choice: $option")
         }
@@ -38,24 +37,23 @@ fun mainMenu() = readNextInt(
          > |   2 -> List Events                                |
          > |   3 -> Update an Event                            |
          > |   4 -> Delete an Event                            |
-         > |   5 -> Archive an Event                           |
          > |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| 
          > |               -BOOKING MENU-                      | 
-         > |   6 -> Add Booking to an Event                    |
-         > |   7 -> Update Booking contents on an Event        |
-         > |   8 -> Delete Booking from an Event               |
-         > |   9 -> Mark Booking as Paid or Unpaid             | 
+         > |   5 -> Add Booking to an Event                    |
+         > |   6 -> Update Booking contents on an Event        |
+         > |   7 -> Delete Booking from an Event               |
+         > |   8 -> Mark Booking as Paid or Unpaid             | 
          > |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| 
          > |          -REPORT MENU FOR EVENTS-                 | 
-         > |   10 -> Search for all Events                     |
-         > |   11 -> Search by Event Category                  |
-         > |   12 -> Search by Duration                        |
-         > |   13 -> Search by Ticket Price                    |
+         > |   9 -> Search for all Events                     |
+         > |   10 -> Search by Event Category                  |
+         > |   11 -> Search by Duration                        |
+         > |   12 -> Search by Ticket Price                    |
          > |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| 
          > |          -REPORT MENU FOR BOOKINGS-               |                                
-         > |   14 -> Search for all Bookings                   |
-         > |   15 -> Search by Payment Status                  |
-         > |   16 -> Search Booking by Date                    |
+         > |   13 -> Search for all Bookings                   |
+         > |   14 -> Search by Payment Status                  |
+         > |   15 -> Search Booking by Date                    |
          > -----------------------------------------------------  
          > |   0) Exit                                         |
          > -----------------------------------------------------  
