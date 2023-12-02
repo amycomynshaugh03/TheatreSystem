684c4d23a342ccf1adb4c309857dcf246781815e
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 15:28:45 +0000

    add event and delete event added to TheatreAPI

--- src/main/kotlin/controllers/TheatreAPI.kt
+++ src/main/kotlin/controllers/TheatreAPI.kt
@@ -1,4 +1,45 @@
 package controllers
 
-class TheatreAPI {
+import models.Booking
+import models.Event
+import utils.Utilities.formatListString
+import java.util.ArrayList
+class TheatreAPI() {
+    private var events = ArrayList<Event>()
+    private var lastId = 0
+    private fun getId() = lastId++
+
+    //CRUD FOR EVENT
+    fun add(event: Event): Boolean {
+        event.eventId = getId()
+        return event.add(event)
+    }
+
+    fun delete(id: Int) = events.removeIf { note -> note.eventId == id }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
