a03f7bc42e4d1d63f20d44b5dd200f5cb8d92f0f
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-02 13:51:25 +0000

    Adding Booking data to Booking data class

--- src/main/kotlin/models/Booking.kt
+++ src/main/kotlin/models/Booking.kt
@@ -1,4 +1,11 @@
 package models
 
-class Booking {
-}
+data class Booking(var bookingId: Int = 0,
+                   var bookingPerformance: String,
+                   var bookingDate: String,
+                   var bookingTime: String,
+                   var customerName: String,
+                   var customerPhone: Int = 0,
+                   var paymentMethod: String,
+                   var paymentStatus: String
+)
