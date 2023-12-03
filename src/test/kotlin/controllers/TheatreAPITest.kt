b19b62595c7935ac93b608368673a8486a794e81
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-03 17:17:57 +0000

    JUnit updateEvents & YAML persistence testing added

--- src/test/kotlin/controllers/TheatreAPITest.kt
+++ src/test/kotlin/controllers/TheatreAPITest.kt
@@ -21,9 +21,9 @@ class TheatreAPITest {
 @BeforeEach
 fun setup() {
     event1 = Event(0,"Variations", "Comedy", "Breakfast & family drama", 13, 10, 60)
-    event2 = Event(0, "Shrek the Musical", "Comedy", "shrek in musical form", 18, 50, 120)
-    event3 = Event(0, "Maltida", "Enlighting", "Orphange Girl being adopted by a rich family", 10, 25, 120)
-    event4 = Event(0, "Talent Show", "Comedy", "People show their talents to the world", 10,30,60)
+    event2 = Event(1, "Shrek the Musical", "Comedy", "shrek in musical form", 18, 50, 120)
+    event3 = Event(2, "Maltida", "Enlighting", "Orphange Girl being adopted by a rich family", 10, 25, 120)
+    event4 = Event(3, "Talent Show", "Comedy", "People show their talents to the world", 10,30,60)
 
     populatedEvents!!.add(event1!!)
     populatedEvents!!.add(event2!!)
@@ -100,15 +100,82 @@ fun setup() {
         }
     }
 
+    @Nested
+    inner class UpdateEvents {
+        @Test
+        fun `updating a event that does not exist returns false`() {
+            assertFalse(
+                populatedEvents!!.update(
+                    4,
+                    Event(0, "Talent Show", "Comedy", "People show their talents to the world", 10, 30, 60)
+                )
+            )
+            assertFalse(
+                populatedEvents!!.update(
+                    -1,
+                    Event(0, "Talent Show", "Comedy", "People show their talents to the world", 10, 30, 60)
+                )
+            )
+            assertFalse(emptyEvents!!.update(0, Event(0, "Talent Show", "Comedy", "People show their talents to the world", 10, 30, 60)))
+        }
 
+        @Test
+        fun `updating a event that exists returns true and updates`() {
+           assertEquals(event4, populatedEvents!!.findEvent(3))
+            assertEquals("Talent Show", populatedEvents!!.findEvent(3)!!.eventTitle)
+            assertEquals("Comedy", populatedEvents!!.findEvent(3)!!.eventCategory)
+            assertEquals("People show their talents to the world", populatedEvents!!.findEvent(3)!!.eventDescription)
+            assertEquals(10, populatedEvents!!.findEvent(3)!!.ageRating)
+            assertEquals(30, populatedEvents!!.findEvent(3)!!.ticketPrice)
+            assertEquals(60, populatedEvents!!.findEvent(3)!!.eventDuration)
 
-
-
-
-
-
-
-
-
-
+            assertTrue(populatedEvents!!.update(3, Event(3, "Updating Event","Hilarious", "Showcasing different talents to four judges, who will win?", 12, 35, 90)))
+            assertEquals("Updating Event", populatedEvents!!.findEvent(3)!!.eventTitle)
+            assertEquals("Hilarious", populatedEvents!!.findEvent(3)!!.eventCategory)
+            assertEquals("Showcasing different talents to four judges, who will win?", populatedEvents!!.findEvent(3)!!.eventDescription)
+            assertEquals(12, populatedEvents!!.findEvent(3)!!.ageRating)
+            assertEquals(35, populatedEvents!!.findEvent(3)!!.ticketPrice)
+            assertEquals(90, populatedEvents!!.findEvent(3)!!.eventDuration)
+        }
     }
+    @Nested
+    inner class PersistenceTests {
+        @Test
+        fun `saving and loading an empty collection in YAML doesn't crash app`() {
+            val storingEvents = TheatreAPI(YAMLSerializer(File("events.yaml")))
+            storingEvents.store()
+
+            val loadedEvents = TheatreAPI(YAMLSerializer(File("events.yaml")))
+            loadedEvents.load()
+
+            assertEquals(0, storingEvents.numberOfEvents())
+            assertEquals(0, loadedEvents.numberOfEvents())
+            assertEquals(storingEvents.numberOfEvents(), loadedEvents.numberOfEvents())
+        }
+
+        @Test
+        fun `saving and loading a loaded collection in YAML doesn't lose data`() {
+            val storingEvents = TheatreAPI(YAMLSerializer(File("events.yaml")))
+            storingEvents.add(event1!!)
+            storingEvents.add(event2!!)
+            storingEvents.add(event3!!)
+            storingEvents.store()
+
+            val loadedEvents = TheatreAPI(YAMLSerializer(File("events.yaml")))
+            loadedEvents.load()
+
+            assertEquals(3, storingEvents.numberOfEvents())
+            assertEquals(3, loadedEvents.numberOfEvents())
+            assertEquals(storingEvents.numberOfEvents(), loadedEvents.numberOfEvents())
+            assertEquals(storingEvents.findEvent(0), loadedEvents.findEvent(0))
+            assertEquals(storingEvents.findEvent(1), loadedEvents.findEvent(1))
+            assertEquals(storingEvents.findEvent(2), loadedEvents.findEvent(2))
+        }
+    }
+
+
+
+
+
+
+}
