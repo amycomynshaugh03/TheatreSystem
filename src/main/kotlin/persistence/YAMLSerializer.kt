d92aac3619cc3566495cdecb8d376f4241a8dfff
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-03 02:09:39 +0000

    adding YAMLSerializer

--- src/main/kotlin/persistence/YAMLSerializer.kt
+++ src/main/kotlin/persistence/YAMLSerializer.kt
@@ -1,4 +1,26 @@
 package persistence
 
-class YAMLSerializer {
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
+class YAMLSerializer(private val file: File): Serializer {
+
+    @Throws(Exception::class)
+    override fun read(): Any? {
+        val yaml = Yaml()
+        val read = FileReader(file)
+        val obj = yaml.load(read) as Any
+        read.close()
+        return obj
+    }
+
+    @Throws(Exception::class)
+    override fun write(obj: Any?) {
+        val yaml = Yaml()
+        val outputStream = FileWriter(file)
+        yaml.dump(obj, outputStream)
+        outputStream.close()
+    }
 }
