a6973ef52d49c3a4fd6c872555f65f9cf7b82c23
Amy Comyns Haugh <comynsamy03@gmail.com>
2023-12-03 01:58:43 +0000

    adding into interface - Serializer

--- src/main/kotlin/persistence/Serializer.kt
+++ src/main/kotlin/persistence/Serializer.kt
@@ -1,4 +1,25 @@
 package persistence
 
+/**
+ * The `Serializer` interface defines the contract for classes that provide
+ * serialization and deserialization functionality for data storage and retrieval.
+ */
 interface Serializer {
+    /**
+     * Writes an object to a data source.
+     *
+     * @param obj The object to be serialized and written to the data source.
+     * @throws Exception if an error occurs during the write operation.
+     */
+    @Throws(Exception::class)
+    fun write(obj: Any?)
+
+    /**
+     * Reads an object from a data source and deserializes it.
+     *
+     * @return The deserialized object, or `null` if no data is found.
+     * @throws Exception if an error occurs during the read operation.
+     */
+    @Throws(Exception::class)
+    fun read(): Any?
 }
